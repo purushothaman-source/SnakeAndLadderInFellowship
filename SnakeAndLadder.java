347170ebe8f439dd78339ae1f9d41f8e445bd9a0
purushothaman-source <purushoth99jun@gmail.com>
2021-01-11 10:27:51 +0530

    ADDED : initialised one player at 0th position

--- SnakeAndLadder.java
+++ SnakeAndLadder.java
@@ -3,5 +3,7 @@ package com.javaprograms;
 public class SnakeAndLadder {
     public static void main(String[] args) {
         System.out.println("welcome to snake and ladder game");
+        final int INITIAL_POSITION=0;
+        final int NO_OF_PLAYERS=1;
     }
 }
