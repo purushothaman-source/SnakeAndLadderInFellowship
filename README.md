ff53d843b7bf3b89af2e4256e8650d6141ed000b
purushothaman-source <purushoth99jun@gmail.com>
2021-01-11 10:21:55 +0530

    ADDED :welcome to snake ladder

---
+++ README.md
@@ -0,0 +1,1 @@
+This Project is used to Play Snake and Ladder Game
---
+++ SnakeAndLadder.java
@@ -0,0 +1,7 @@
+package com.javaprograms;
+
+public class SnakeAndLadder {
+    public static void main(String[] args) {
+        System.out.println("welcome to snake and ladder game");
+    }
+}
