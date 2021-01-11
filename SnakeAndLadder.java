c2e2020c153a7568e001b016da202b98fa5576f0
purushothaman-source <purushoth99jun@gmail.com>
2021-01-11 11:15:42 +0530

    ADDED :functionality to run till winning position

--- SnakeAndLadder.java
+++ SnakeAndLadder.java
@@ -7,28 +7,34 @@ public class SnakeAndLadder {
         System.out.println("welcome to snake and ladder game");
         final int INITIAL_POSITION = 0;
         final int NO_OF_PLAYERS = 1;
-        int dieMove = rollDie();
-
         Random random = new Random();
-        int option = random.nextInt(3);
         final int NO_PLAY = 0;
         final int LADDER = 1;
         final int SNAKE = 2;
         int currentPosition = INITIAL_POSITION;
+        int winningPosition = 100;
 
-        switch (option) {
-            case NO_PLAY:
-                currentPosition += 0;
-                break;
-            case LADDER:
-                currentPosition = currentPosition + dieMove;
-                break;
-            case SNAKE:
-                if ((currentPosition - dieMove) <= 0)
-                    currentPosition = 0;
-                else
-                    currentPosition = currentPosition - dieMove;
-                break;
+        while (currentPosition < winningPosition) {
+            int option = random.nextInt(3);
+            int dieMove = rollDie();
+            switch (option) {
+                case NO_PLAY:
+                    currentPosition += 0;
+                    System.out.println(">>>>>>NoPlay");
+                    break;
+                case LADDER:
+                    currentPosition = currentPosition + dieMove;
+                    System.out.println(">>>>>>U got ladder");
+                    break;
+                case SNAKE:
+                    if ((currentPosition - dieMove) <= 0)
+                        currentPosition = 0;
+                    else{
+                        currentPosition = currentPosition - dieMove;
+                        System.out.println(">>>>>>you bitten by snake");}
+                    break;
+            }
+            System.out.println("PlayerPosition = " + currentPosition);
         }
     }
 
