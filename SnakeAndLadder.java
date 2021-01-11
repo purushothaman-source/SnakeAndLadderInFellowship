ab817a17a13865b6e536be56b765f5707d9f7c73
purushothaman-source <purushoth99jun@gmail.com>
2021-01-11 12:01:27 +0530

    ADDED :code functionality to play with two players

--- SnakeAndLadder.java
+++ SnakeAndLadder.java
@@ -3,50 +3,96 @@ package com.javaprograms;
 import java.util.Random;
 
 public class SnakeAndLadder {
+    static int choice = 0;
+
     public static void main(String[] args) {
-        System.out.println("welcome to snake and ladder game");
-        final int INITIAL_POSITION = 0;
-        final int NO_OF_PLAYERS = 1;
-        Random random = new Random();
-        final int NO_PLAY = 0;
-        final int LADDER = 1;
-        final int SNAKE = 2;
-        int currentPosition = INITIAL_POSITION;
-        int winningPosition = 100;
-        int timeDiesRolled = 0;
+        System.out.println("WELCOME TO SNAKE AND LADDER GAME");
+        int player1rolled = 0;
+        int player2rolled = 0;
+        int player1Position = 0;
+        int player2Position = 0;
 
-
-        while (currentPosition < winningPosition) {
-            int option = random.nextInt(3);
-            int dieMove = rollDie();
-            timeDiesRolled++;
-            switch (option) {
-                case NO_PLAY:
-                    currentPosition += 0;
-                    System.out.println(">>>>>>NoPlay");
-                    break;
-                case LADDER:
-                    if((currentPosition+dieMove)>winningPosition)
-                        currentPosition=currentPosition;
-                    else{
-                    currentPosition = currentPosition + dieMove;
-                    System.out.println(">>>>>>U got ladder");}
-                    break;
-                case SNAKE:
-                    if ((currentPosition - dieMove) <= 0)
-                        currentPosition = 0;
-                    else{
-                        currentPosition = currentPosition - dieMove;
-                        System.out.println(">>>>>>you bitten by snake");}
-                    break;
+        while (true) {
+            player1Position = snakeAndLadderGame(player1Position);
+            player1rolled++;
+            if (player1Position == 100) {
+                System.out.println(
+                        "PLAYER1POSITION :" + player1Position + ">>>>>>>  " + "PLAYER2POSITION :" + player2Position);
+                System.out.println("PLAYER1 WON BY ROLLED : " + player1rolled + " times");
+                break;
             }
-            System.out.println("PlayerPosition = " + currentPosition);
+
+            while (choice == 1) {
+
+                player1Position = snakeAndLadderGame(player1Position);
+                player1rolled++;
+            }
+            if (player1Position == 100) {
+                System.out.println(
+                        "PLAYER1POSITION :" + player1Position + ">>>>>>>  " + "PLAYER2POSITION :" + player2Position);
+                System.out.println("PLAYER1 WON BY ROLLED : " + player1rolled + " times");
+                break;
+            }
+
+            player2Position = snakeAndLadderGame(player2Position);
+            if (player2Position == 100) {
+                System.out.println(
+                        "PLAYER1POSITION :" + player1Position + ">>>>>>>  " + "PLAYER2POSITION :" + player2Position);
+                System.out.println("PLAYER2 WON BY ROLLED : " + player2rolled + " times");
+                break;
+            }
+            player2rolled++;
+
+            while (choice == 1) {
+
+                player2Position = snakeAndLadderGame(player2Position);
+                player2rolled++;
+
+            }
+            if (player2Position == 100) {
+                System.out.println(
+                        "PLAYER1POSITION :" + player1Position + ">>>>>>>  " + "PLAYER2POSITION :" + player2Position);
+                System.out.println("PLAYER2 WON BY ROLLED : " + player2rolled + " times");
+                break;
+            }
+
+
         }
-        System.out.println("******PlayerRolled "+timeDiesRolled+" times to reach Winning Position*****");
+
+
     }
 
     public static int rollDie() {
         Random random = new Random();
         return random.nextInt(6) + 1;
     }
+
+    public static int snakeAndLadderGame(int playerposition) {
+        Random random = new Random();
+        final int NO_PLAY = 0;
+        final int LADDER = 1;
+        final int SNAKE = 2;
+        choice = random.nextInt(3);
+        int dieMove = rollDie();
+        switch (choice) {
+            case NO_PLAY:
+                playerposition += 0;
+                break;
+            case LADDER:
+                if ((playerposition + dieMove) > 100)
+                    playerposition = playerposition ;
+                else
+                    playerposition = playerposition + dieMove;
+                break;
+            case SNAKE:
+                if ((playerposition - dieMove) < 0)
+                    playerposition = 0;
+                else
+                    playerposition = playerposition - dieMove;
+                break;
+        }
+        return playerposition;
+
+    }
+
 }
