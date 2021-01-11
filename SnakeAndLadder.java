31d38d3f337afaf6a9cc7b25de66cf44f5ef5334
purushothaman-source <purushoth99jun@gmail.com>
2021-01-11 10:33:45 +0530

    ADDED :code to get diemove

--- SnakeAndLadder.java
+++ SnakeAndLadder.java
@@ -1,9 +1,16 @@
 package com.javaprograms;
 
+import java.util.Random;
+
 public class SnakeAndLadder {
     public static void main(String[] args) {
         System.out.println("welcome to snake and ladder game");
         final int INITIAL_POSITION=0;
         final int NO_OF_PLAYERS=1;
+        int dieMove=rollDie();
+    }
+    public static int rollDie() {
+        Random random =new Random();
+        return random.nextInt(6)+1;
     }
 }
